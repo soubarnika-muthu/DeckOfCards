bb7c42a59aec7aa628377d598e09bfa03b8ff0d7
soubarnika-muthu <venkatachalapathymuthu@gmail.com>
2021-07-08 22:04:21 +0530

    [Soubarnika]Shuffling and Distributing the cards

---
+++ DeckOfCards/Cards.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeckOfCards
+{
+   public class Cards
+    {
+        public string suit;
+        public string rank;
+        public Cards(string suit, string rank)
+        {
+            this.suit = suit;
+            this.rank = rank;
+        }
+    }
+}
--- DeckOfCards/Program.cs
+++ DeckOfCards/Program.cs
@@ -7,7 +7,15 @@ namespace DeckOfCards
         static void Main(string[] args)
         {
             Console.WriteLine("Implementation  of Deck of Cards Program");
-
+            Console.WriteLine("Welcome to Deck of Cards");
+            //creating object for deck of card class
+            DeckOfCards deckOfCards = new DeckOfCards();
+            //initialize the player array with 4 players and each player with 9 cards
+            Cards[,] players = new Cards[4, 9];
+            //call the alotCards method
+            players = deckOfCards.alotCards(players);
+            deckOfCards.display(players);
+            Console.Read();
         }
     }
 }
